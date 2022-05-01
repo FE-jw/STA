1ec5628624a10eefaba13968dc3fdf00152cedd4
JW <zaixu91@gmail.com>
2022-05-01 21:57:50 +0900

    Release Version 1.2

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <a href="https://fe-jw.github.io/STA" target="_blank"><img src="https://cdn.jsdelivr.net/gh/fe-jw/STA/img/sta_logo.jpg" width="100%" align="top" style="max-width:830px;"></a>
 
-# **STA(Scroll Trigger Animation)<br>[Ver 1.1 Demo](https://fe-jw.github.io/STA/Ver1.1)**
+# **STA(Scroll Trigger Animation)<br>[Ver 1.2 Demo](https://fe-jw.github.io/STA)**
 
 ## **Property**
 1. data-sta: 기본 설정
@@ -16,6 +16,11 @@
 3. data-sta-duration: 애니메이션 시간(기본값 1000)
 	```html
 	<div data-sta data-sta-duration="2000"></div>
+	```
+
+4. data-sta-offset: 트리거 포인트 설정(기본값 0.8)
+	```html
+	<div data-sta data-sta-duration="0.5"></div>
 	```
 
 ## **Get Started**
@@ -44,12 +49,14 @@
 	});
 	```
 
-## **놀라울 정도로 가벼운 용량(min 기준)**
+## **Amazing Size(min)**
 |Ver|css|js|
 |:------:|:---:|:---:|
+|1.2|347byte|1.05kb|
 |1.1|347byte|1.03kb|
 |1|343byte|966byte|
 
 ## **History**
+- 220501 Released [Ver 1.2](https://fe-jw.github.io/STA/Ver1.2)
 - 220325 Released [Ver 1.1](https://fe-jw.github.io/STA/Ver1.1)
 - 220306 Released [Ver 1](https://fe-jw.github.io/STA/Ver1)
---
+++ Ver1.2/sta.js
@@ -0,0 +1,57 @@
+/*!
+	* Version: 1.2
+	* Web: https://fe-jw.github.io/STA/Ver1.2
+	* Github: https://github.com/FE-jw/STA
+	* Released: 2022-05-01
+*/
+
+NodeList.prototype.STA = function(){
+	if(window.NodeList && !NodeList.prototype.forEach){
+		NodeList.prototype.forEach = Array.prototype.forEach;
+	}
+
+	var eleAni = this;
+	var onInit = function(){
+		eleAni.forEach(function(e, idx){
+			//Settings
+			if(!e.classList.contains('sta-complete')){
+				var eleTop = window.scrollY + e.getBoundingClientRect().top;
+				var eleBottom = eleTop + e.offsetHeight;
+				var eleTrigger = eleTop + (e.offsetHeight * (e.dataset.staOffset || 0.8));
+
+				//Duration 있는 경우
+				if(e.dataset.staDuration && e.style.transitionDuration == ''){
+					e.style.transitionDuration = e.dataset.staDuration / 1000 + 's';
+				}
+
+				//Trigger Active
+				if(window.scrollY + window.innerHeight >= eleTrigger && window.scrollY < eleBottom){
+					if(!e.classList.contains('sta-trigger')){
+						if(e.dataset.staDelay){
+							//Delay 있는 경우
+							setTimeout(function(){
+								e.classList.add('sta-trigger');
+							}, e.dataset.staDelay);
+						}else{
+							//Delay 없는 경우
+							e.classList.add('sta-trigger');
+						}
+					}
+				}
+				
+				//Complete
+				e.addEventListener('transitionend', function(){
+					if(e.classList.contains('sta-trigger') && !e.classList.contains('sta-complete')){
+						e.classList.remove('sta-trigger');
+						e.classList.add('sta-complete');
+					}
+				});
+			}
+		});
+	}
+
+	onInit();
+
+	window.addEventListener('resize', onInit);
+	window.addEventListener('scroll', onInit);
+};
